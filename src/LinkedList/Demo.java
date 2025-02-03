0cf7690cc4c3832d726ca483056153fd8ab33ae0
WebWithRathor <devrajrathor143@gmail.com>
2025-02-03 23:15:06 +0530

    Demo

---
+++ src/LinkedList/Demo.java
@@ -0,0 +1,10 @@
+package LinkedList;
+
+public class Demo {
+    public static void main(String[] args) {
+        MyLinkedList obj = new MyLinkedList(1);
+        obj.addFirst(3);
+        obj.add(5,1);
+        System.out.println(obj);
+    }
+}
