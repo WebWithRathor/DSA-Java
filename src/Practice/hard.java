c3910b239496081d4ef70dd5233a688d9bcf7730
WebWithRathor <devrajrathor143@gmail.com>
2025-02-09 22:45:16 +0530

    hard.java

---
+++ src/Practice/hard.java
@@ -0,0 +1,41 @@
+package Practice;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class hard {
+    public static void main(String[] args) {
+        String s = "Programming";
+
+//        HashSet<Character> set = new HashSet<>();
+//        for(char ch : s.toCharArray())set.add(ch);
+//
+//        System.out.println(set);
+//        System.out.println(set.size());
+
+
+//        print duplicates
+//        HashSet<Integer> set = new HashSet<>();
+//        int []arr = {1, 2, 3, 1,1, 4, 2};
+//        for(int i :arr){
+//            if(!set.add(i)) set2.add(i);
+//        }
+//        System.out.println(set2);
+
+
+//        symmetric diffrence
+//        HashSet<Integer> set1 = new HashSet<>(Arrays.asList(1,2,3,4));
+//        HashSet<Integer> set2 = new HashSet<>(Arrays.asList(3,4,5,6));
+//
+//        for(int a : set1){
+//            if(!set2.add(a))set2.remove(a);
+//        }
+//        System.out.println(set2);
+
+        
+
+
+
+
+    }
+}
